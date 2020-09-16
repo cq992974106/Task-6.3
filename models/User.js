2f94ca95ff51a4ce8beacc0153ed244fa2fc80e1
cq992974106 <70575714+cq992974106@users.noreply.github.com>
2020-09-16 17:54:33 +0800

    make it better

---
+++ models/User.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
+var bcrypt = require('bcrypt-nodejs');
+var bcrypt = require('bcrypt');
+SALT_WORK_FACTOR = 5;
+
+const userSchema=new mongoose.Schema(
+    {
+    id: String,
+    username: {
+      type: String,
+    },
+    country: {
+    type: String,
+    
+  },
+ 
+    firstName: {
+    type: String,
+    
+  },
+    lastName: {
+    type: String,
+    
+  },
+    email: {
+    type: String,
+    
+  },
+    password: {
+    type: String,
+   
+  },
+    address1: {
+    type: String,
+   
+  },
+  address2: {
+    type: String,
+  
+  },
+    city: {
+    type: String,
+   
+  },
+    state: {
+    type: String,
+  
+  },
+    postalCode: {
+    type: Number,
+  },
+    phoneNumber: {
+    type: Number,
+  }
+    }
+)
+
+
+//userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose, {
+  usernameUnique: false})
+
+
+module.exports = mongoose.model("User",userSchema)
--- server.js
+++ server.js
@@ -1,100 +1,177 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const User = require('./models/User')
 const mongoose = require("mongoose")
 const validator = require("validator")
 const https = require("https")
 var crypto = require("crypto");
 var path = require('path')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+const session = require('express-session')
+var LocalStrategy = require('passport-local').Strategy;
+var nodemailer = require("nodemailer");
+var bcrypt = require('bcrypt');
+SALT_WORK_FACTOR = 5;
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true})
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extend:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 
 
 app.engine('html', require('express-art-template'))
 app.use('/public/', express.static(path.join(__dirname, './public/')))
 app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
 
+//session
+app.use(session({
+  cookie:{maxAge: 120000},
+  resave:false,
+  saveUninitialized:false,
+  secret:'$$$DeakinSecret'
+}))
+app.use(passport.initialize())
+app.use(passport.session());
+
+//passport config
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 //route
 app.get('/', function (req, res){
-  res.render('index.html')
+  res.render('login.html', { user : req.user })
 })
 
-app.post('/', function (req, res){
-})
-
-app.get('/login', function (req, res){
-  res.render('index.html')
-})
 
 app.get('/register', function (req, res){
   res.render('register.html')
 })
 
-app.get('/reqtask', function (req, res){
-  res.render('reqtask.html')
-})
-
-app.post('/reqtask', function (req, res){
-})
-
-//user
-const userSchema=new mongoose.Schema(
-    {
-    id: String,
-    country: {
-    type: String,
-    required: true
-  },
-    firstName: {
-    type: String,
-    required: true
-  },
-    lastName: {
-    type: String,
-    required: true
-  },
-    email: {
-    type: String,
-    required: true
-  },
-    password: {
-    type: String,
-    required: true
-  },
-    address1: {
-    type: String,
-    required: true
-  },
-  address2: {
-    type: String,
-    required: true
-  },
-    city: {
-    type: String,
-    required: true
-  },
-    state: {
-    type: String,
-    required: true
-  },
-    postalCode: {
-    type: Number,
-  },
-    phoneNumber: {
-    type: Number,
+app.get('/iCrowdTask',(req, res)=>{
+  
+  if(req.isAuthenticated()
+    )
+    { res.render('iCrowdTask.html')
+    console.log('success!')
   }
-    }
-)
+    else
+    {res.redirect('/')}
+  })
 
-const User = new mongoose.model("User", userSchema)
+app.post('/iCrowdTask', function (req, res){
+})
+
+app.get('/googleLog', function (req, res){
+  res.render('googleSignIn.html')
+})
+//forget password
+app.get('/forgetPwd', function (req, res){
+  res.render('forgetPwd.html')
+})
+
+app.post('/forgetPwd', function (req, res){
+  var email = req.body.email;
+  User.findOne({
+    email: email},function (err, user){
+
+      if(err) { 
+        console.log('err')
+        return res.status(500).json({
+            err_code:500,
+            message: 'err'
+        })
+    }
+    
+     if(!user) {
+      return res.status(200).json({
+          err_code: 1,
+          message: 'email not existed'
+      })
+  }
+  //send reset email
+  var smtpTransport = nodemailer.createTransport({
+    host: 'smtp.qq.com',//qq email server
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+    user: '992974106@qq.com',
+    pass: 'djzwqsmcfdlvbeji'
+    }
+    });
+    
+    var mailOptions = {
+    　　from: "992974106@qq.com",
+    　　to: email,
+    　　subject: "iCrowdTask password reset",
+    　　html:"<h1>Hi,"+email+"</h1><br> <p>click <a href="+'"http://localhost:5000/forgetPwd/resetPwd"'+">here</a> to reset your password</p>"
+    }
+
+    smtpTransport.sendMail(mailOptions, function(err, resp){
+      　　if(err){
+      　　　　console.log(err)
+      　　}
+      console.log("send successfully")
+      　　smtpTransport.close();//close connect
+      });
+  return res.status(200).json({
+    err_code: 0,
+    message: 'send reset email successfully'
+})
+    })
+
+
+})
+
+
+//reset password
+app.get('/forgetPwd/resetPwd', function (req, res){
+  res.render('resetPwd.html')
+})
+app.post('/forgetPwd/resetPwd', function (req, res){
+  var email = req.body.email;
+  var psw = req.body.password;
+
+
+  User.findOne({
+    email: email},function (err, user){
+
+      if(err) { 
+        return res.status(500).json({
+            err_code:500,
+            message: 'err'
+        })
+    }
+    
+     if(!user) {
+      return res.status(200).json({
+          err_code: 1,
+          message: 'email not existed'
+      })
+  }
+
+  user.setPassword(psw,  function(){
+    user.save();
+    return res.status(200).json({
+      err_code: 0,
+      message: 'reset password successfully'
+  })
+  })
+  
+    })
+
+})
+
+
 //insert function
-function insert(id,country,fName,lName,email,psw,adress1,adress2,city,state,zip,pNumber){
+function insert(id,username,country,fName,lName,email,psw,adress1,adress2,city,state,zip,pNumber){
 
 var user =  new User({
   id: id,
+  username:username,
   country: country,
     firstName: fName,
     lastName: lName,
@@ -128,6 +205,7 @@ app.post('/register', function (req, res) {
   
   var id = req.body.first_name+req.body.email;
   var country = req.body.country;
+  var username = req.body.email;
   var fName = req.body.first_name;
   var lName = req.body.last_name;
   var email = req.body.email;
@@ -144,8 +222,6 @@ app.post('/register', function (req, res) {
   
   var md5 = crypto.createHash("md5");
   var newPas = md5.update(psw).digest("hex");
-
-
 
 
   User.find(updatestr, function(err, obj){
@@ -183,7 +259,28 @@ app.post('/register', function (req, res) {
       }
       else
       {
-        insert(id,country,fName,lName,email,newPas,adress1,adress2,city,state,zip,pNumber); 
+        User.register(new User({id:id,username: username,
+          country : req.body.country,
+          firstName : req.body.first_name,
+          lastName : req.body.last_name,
+          email : req.body.email,
+          adress1 : req.body.address1,
+          adress2 : req.body.address2,
+          city : req.body.city,
+          state : req.body.state,
+          postalCode : req.body.zip,
+          phoneNumber : req.body.phone_number }), req.body.password, function(err, User) {
+          if (err) {
+            console.log("err")
+              return res.render('/register', { User : User });
+          }
+  
+          passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+          });
+      });
+
+        //insert(id,username,country,fName,lName,email,newPas,adress1,adress2,city,state,zip,pNumber); 
             //send email
      const data = {
        members:[{
@@ -222,45 +319,62 @@ app.post('/register', function (req, res) {
 })  
 });
 
-app.post('/login', function (req, res, next) {
 
+
+app.post('/', passport.authenticate('local'), function(req, res) {
+  res.redirect('/iCrowdTask');
+});
+
+
+
+
+/*
+app.post('/',function (req, res, next) {
+
+  
+  
   res.setHeader('Content-type','application/json;charset=utf-8')
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By",' 3.2.1')
 
-  var email = req.body.email;
+  var email = req.body.username;
   var UserPsw = req.body.password;
 
   var md5 = crypto.createHash("md5");
   var newPas = md5.update(UserPsw).digest("hex");
 
+ 
+
 
   User.findOne({
       email: email,password:newPas},function (err, user){
-    if(err) {
-
+        console.log(email+" "+newPas)
+    if(err) { 
+        console.log('err')
         return res.status(500).json({
             err_code:500,
             message: 'err'
         })
     }
-
-    if(!user) {
+    
+     if(!user) {
       return res.status(200).json({
           err_code: 1,
           message: 'email or password err'
       })
   }
-    return res.status(200).json({
-             err_code: 0,
-             message: 'success'
-         })
-    
+   return res.status(200).json({
+    err_code: 0,
+    message: 'registered successfully'
+})
     })
-});
+  
 
+}); 
+ 
+*/
 // Crowd REST APIs
 //workers
 app.route('/workers')
@@ -273,11 +387,11 @@ app.route('/workers')
 .post( (req,res)=>{
     const user = new User({
         id : req.body.first_name+req.body.email,
+        username:req.body.email,
         country : req.body.country,
         firstName : req.body.first_name,
         lastName : req.body.last_name,
         email : req.body.email,
-        password : req.body.password,
         adress1 : req.body.address1,
         adress2 : req.body.address2,
         city : req.body.city,
@@ -310,6 +424,7 @@ app.route('/workers/:id')
 User.update(
     {id: req.params.id},
     {   id: req.body.first_name+req.body.email,
+        username:req.body.email,
         country : req.body.country,
         firstName : req.body.first_name,
         lastName : req.body.last_name,
@@ -349,7 +464,7 @@ User.update(
 })
 
 app.listen(process.env.PORT || 5000, ()=>{
-  console.log('Server started on port 8000');
+  console.log('Server started on port 5000');
 })
 
 
